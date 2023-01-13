626e607a681cae00faae88a28f221ac087e1d2b9
Drenajnayavoda <91183133+Drenajnayavoda@users.noreply.github.com>
2023-01-13 05:04:45 +0300

    Create 3.sh

---
+++ bestof3/3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+read -p "Please write your phrase: " -r node
+read -p "Please write your shift: " -r shft
+
+declare -a str
+
+while IFS="" read -r -n 1 x; do
+  str+=("$x")
+done <<<$node
+
+res=()
+
+for ((i = 0; i < ${#str[@]}; i++)); do
+
+  ascii=$(printf "%d" "'${str[$i]}'")
+
+  if (((($ascii >= 48)) && (($ascii <= 57)))); then
+
+    res[$i]=$((9 - ${str[$i]}))
+
+  elif (((($ascii >= 65)) && (($ascii <= 90)))); then
+
+    num=$(($shft + $ascii - 64))
+    new=$(($num % 26 + 64))
+
+    if (($i % 2 != 0)); then
+      new=$(($new + 32))
+    fi
+
+    res[$i]=$(printf "\\$(printf '%03o' "$new")")
+
+  else
+
+    res[$i]=${str[$i]}
+
+  fi
+done
+
+for ((i = $((${#res[@]} - 1)); i >= 0; i--)); do
+  if (($(printf "%d" "'${str[$i]}'") == 32)); then
+
+    echo -n " "
+
+  else
+
+    echo -n ${res[$i]}
+
+  fi
+done
+
+echo
+
+exit 0
